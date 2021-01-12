fe1e51777af7b6040b4a8cf4390d0e113b826e97
JDC1492 <Jonathan_Cohen10@yahoo.com>
2021-01-12 12:36:16 -0500

    db re-seed for working photo imgs

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,5 @@
 class Game < ApplicationRecord
     validates :title, :release_year, :console, :developer, :genre,  :presence => true
+    validates :title, uniqueness: true
     validates :release_year, :length => { :is => 4 }
 end
